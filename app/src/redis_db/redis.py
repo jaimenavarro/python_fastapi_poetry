6a0254b8f0598e90b02dc9e324b77e9788442c3b
jn245147 <jaime.x.navarrosantapau@gsk.com>
2022-12-07 19:08:29 +0100

    Initial commit

---
+++ app/src/redis_db/redis.py
@@ -0,0 +1,12 @@
+import redis
+import json
+
+db_redis = redis.Redis(host='redis', port=6379, db=0)
+
+
+def get_redis_value(key):
+    return json.loads(db_redis.get(key))
+
+
+def set_redis_value(key, value):
+    return db_redis.set(key, value.json())
