dbaa86b87b9bee977643ce30ae6122c521821aff
jaimenavarro <jaime.navarro.santapau@gmail.com>
2022-12-13 11:46:02 +0100

    Fixed API documentation

--- app/src/main.py
+++ app/src/main.py
@@ -9,17 +9,16 @@ from app.src.redis_db.redis import db_redis
 models.Base.metadata.create_all(bind=engine)
 
 # Creates an application instance.
-app = FastAPI()
+app_name = "FastAPI App with Pyctuator"
+app = FastAPI(title=app_name)
 
 # Add routes to the application
 app.include_router(users.router)
 app.include_router(admin.router)
 
-app_name = "FastAPI App with Pyctuator"
-app = FastAPI(title=app_name)
-
 # Load and configure actuator
 actuator_api = actuators.ActuatorAPI(app)
 actuator_api.register_sql_db_health_provider(engine)
 actuator_api.register_redis_db_health_provider(db_redis)
+
 
